aed3742284129b4309f604e563d6d29dfe9e526d
micmac <mike@mikebarb.net>
2017-06-10 12:16:02 +0000

    change ajax host names for heroku

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -60,7 +60,7 @@ $(document).ready(function() {
             console.log("myuser_id:" + myuser_id);
             $.ajax({
                 type: 'POST',
-                url: "https://baker-micmac.c9users.io/orders",
+                url: "https://hidden-sea-17611.herokuapp.com/orders",
                 data: {
                         order: {
                                   product_id: myproduct_id,
@@ -105,7 +105,7 @@ $(document).ready(function() {
             console.log("myorder_id:" + myorder_id);
             $.ajax({
                 type: 'DELETE',
-                url: "https://baker-micmac.c9users.io/orders/" + myorder_id,
+                url: "https://hidden-sea-17611.herokuapp.com/orders/" + myorder_id,
                 dataType: 'json',
                 success: function(){
                     $(eleorigqty).text("");
@@ -125,7 +125,7 @@ $(document).ready(function() {
             console.log("now call ajax to update quantity on existing record");
             $.ajax({
                 type: 'POST',
-                url: "https://baker-micmac.c9users.io/orders/" + Number($(this).attr("id")),
+                url: "https://hidden-sea-17611.herokuapp.com/orders/" + Number($(this).attr("id")),
     
                 data: {
                         order: {
